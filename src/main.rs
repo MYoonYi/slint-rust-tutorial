9aa717be04dc2691af3fc3d0ca309f8e9facf498
MYoonYi <myoonyi@aol.com>
2025-11-15 09:41:21 +0100

    ✨ feat: Mischung `MemoryTile`-Grid implementieren
    - Tiles über `get-memory_tiles()` abrufen
    - Tiles duplizieren, sodass dasGrid Paare der Elemente enthalten wird
    -  Tiles zufällig mischen
    - Ergebnis mit `set_memory_tiles()` ins Modell zurückschreiben

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,28 @@
 
 slint::include_modules!();
 
-fn main() -> Result<(), slint::PlatformError> {
-    let main_window = MainWindow::new()?;
+fn main() {
+    use slint::Model;
 
-    main_window.run()
+    let main_window = MainWindow::new().unwrap();
+
+    // Tiles aus dem Modell abrufen
+    let mut tiles = main_window
+        .get_memory_tiles()
+        .iter()
+        .collect::<Vec<TileData>>();
+
+    // Duplizieren,um sicherzustellen, dass Paarevorhanden sind.
+    tiles.extend(tiles.clone());
+
+    // Tiles zufällig anordnen
+    use rand::seq::SliceRandom;
+    let mut rng = rand::thread_rng();
+    tiles.shuffle(&mut rng);
+
+    // den gemischten Vec zur Property des Modells zuweisen.
+    let tiles_model = std::rc::Rc::new(slint::VecModel::from(tiles));
+    main_window.set_memory_tiles(tiles_model.clone().into());
+
+    main_window.run().unwrap();
 }
