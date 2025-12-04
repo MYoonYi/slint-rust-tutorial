92991b95a25877390deb3721f4235fffe8b9e2f0
MYoonYi <myoonyi@aol.com>
2025-12-04 17:44:37 +0100

    ✨ feat: Die Callback des Aufdeckens implementieren
     Die Callback-Methode `on_check_if_pair_solved()` wird so implemenitert:
     1. Alle aufgedeckten Kacheln filtern.
     2. Wenn es mindestens 2 gibt, dann die Beide abgleichen (`is_pair_solved`)
     3. Zustände entsprechend des Abgleichs setzen.
      3-1. Wenn die Beide identisch sind, dann deren Zustände `solved` setzen.
      3-2. Wenn nicht, dann die Klick-Interaktion deaktivieren.
      3-2-1. Wenn 1 Sekunde läuft, dann die Interaktion wieder aktivieren sowie die 2 aufgedeckte Kacheln wieder zurücksetzen.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 // Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use slint::{ComponentHandle, Timer};
+use std::time::Duration;
+
 slint::include_modules!();
 
 fn main() {
@@ -14,7 +17,7 @@ fn main() {
         .iter()
         .collect::<Vec<TileData>>();
 
-    // Duplizieren,um sicherzustellen, dass Paarevorhanden sind.
+    // Duplizieren,um sicherzustellen, dass Paare vorhanden sind.
     tiles.extend(tiles.clone());
 
     // Tiles zufällig anordnen
@@ -22,9 +25,48 @@ fn main() {
     let mut rng = rand::thread_rng();
     tiles.shuffle(&mut rng);
 
-    // den gemischten Vec zur Property des Modells zuweisen.
+    // Den gemischten Vec zur Property des Modells zuweisen.
     let tiles_model = std::rc::Rc::new(slint::VecModel::from(tiles));
     main_window.set_memory_tiles(tiles_model.clone().into());
+
+    // Jetzt die Game-Logik implementieren.
+    // - Weak-Pointer erstellen, um Zyklen der Referenzen zu vermeiden.
+    let main_window_weak = main_window.as_weak();
+    // - Die Callback-Funktion definieren, die aufgerufen wird, wenn ein Kachel angeklickt wird.
+    main_window.on_check_if_pair_solved(move || {
+        let mut flipped_tiles = tiles_model
+            .iter()
+            .enumerate()
+            .filter(|(_, tile)| tile.image_visible && !tile.solved);
+
+        if let (
+            Some((tile_number1_index, mut tile_number1)),
+            Some((tile_number2_index, mut tile_number2)),
+        ) = (flipped_tiles.next(), flipped_tiles.next())
+        {
+            let is_pair_solved = tile_number1 == tile_number2;
+            if is_pair_solved {
+                tile_number1.solved = true;
+                tiles_model.set_row_data(tile_number1_index, tile_number1);
+                tile_number2.solved = true;
+                tiles_model.set_row_data(tile_number2_index, tile_number2);
+            } else {
+                // Den Steuern über das Fenster zu haben, um das Klick eines Kachels temporär bzw. für 1 Sekunde zu sperren.
+                let main_window = main_window_weak.unwrap();
+                main_window.set_disable_tiles(true);
+
+                // Jetzt die Interaktion mit den Kacheln wieder (nach 1 Sekunde) freigeben.
+                let tiles_model = tiles_model.clone();
+                Timer::single_shot(Duration::from_secs(1), move || {
+                    main_window.set_disable_tiles(false);
+                    tile_number1.image_visible = false;
+                    tiles_model.set_row_data(tile_number1_index, tile_number1);
+                    tile_number2.image_visible = false;
+                    tiles_model.set_row_data(tile_number2_index, tile_number2);
+                });
+            }
+        }
+    });
 
     main_window.run().unwrap();
 }
